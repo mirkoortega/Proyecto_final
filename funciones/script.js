16f435a0bfd07fa8d4548acf3f21daa5809a4fd0
tania.limachi@ucb.edu.bo <tania@WINDOWS-2T5J4JB.localdomain>
2025-11-20 10:17:10 -0400

    Se modifico leyendas.html y se creo un archivo json de leyendas-mitos con contenido para esa pagina, tambien se trabajo en el javascript

--- funciones/script.js
+++ funciones/script.js
@@ -31,6 +31,38 @@ document.addEventListener("DOMContentLoaded", cargarInicio);
 
 
 
+//Leyendas
+
+const URL_LeyendaMito = '../datos/leyendas-mitos.json'; 
+
+async function cargarLeyenMito() {
+  try {
+    const respuesta = await fetch(URL_LeyendaMito);
+    const data = await respuesta.json();
+
+    // Acceder correctamente al array
+    const primer = data["leyendas-misticas"][0];
+
+    // Obtener elementos (primer elemento de la clase)
+    const tituloEl = document.getElementsByClassName('titulo')[0];
+    const textoEl = document.getElementsByClassName('masa-texto')[0];
+    const imagenEl = document.getElementsByClassName('imagenLM')[0];
+
+    // Asignar contenido
+    tituloEl.textContent = primer.titulo;
+    textoEl.textContent = primer["masa-texto"];
+    imagenEl.src = primer["img-leyenda"];
+
+  } catch (error) {
+    console.error("Error al cargar la información:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", cargarLeyenMito);
+
+
+
+
 // Sobre proyecto
 const URL_PROY = '../datos/sobre-proyecto.json'; 
 
